5830143ebd201214f5e024dcac2326e3ca78ce4d
Coleko22 <colofran23@gmail.com>
2025-09-03 12:07:17 -0300

    saque s de https

--- Front_APINotas/script.js
+++ Front_APINotas/script.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     try {
-      const res = await fetch("https://localhost:8080/api/notes", {
+      const res = await fetch("http://localhost:8080/api/notes", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
@@ -75,7 +75,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const id = notaDiv.dataset.id;
 
             // https://backend-api-notas.onrender.com
-            const res = await fetch(`https://localhost:8080/api/notes/${id}`, {
+            const res = await fetch(`http://localhost:8080/api/notes/${id}`, {
               method: "PUT",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify({
@@ -101,7 +101,7 @@ document.addEventListener("DOMContentLoaded", () => {
       botonBorrar.addEventListener("click", async () => {
         try {
           const id = notaDiv.dataset.id;
-          const res = await fetch(`https://localhost:8080/api/notes/${id}`, {
+          const res = await fetch(`http://localhost:8080/api/notes/${id}`, {
             method: "DELETE"
           });
           if (!res.ok) throw new Error(`HTTP ${res.status}`);
